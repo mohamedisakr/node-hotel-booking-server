ebee27400e211c87f1a1b8583fe74bb31571689b
mohamedisakr <mohamedisakr@gmail.com>
2021-12-29 14:03:29 +0200

    connecting to database

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 require('dotenv').config()
 import express from 'express'
-
+import cors from 'cors'
 import {readdirSync} from 'fs'
+const morgan = require('morgan')
+import mongoose from 'mongoose'
 
 const app = express()
+
+// connect to database
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Server error connecting to MongoDB:', error)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(morgan('combined'))
 
 readdirSync('./routes').map((r) => app.use('/api/v1', require(`./routes/${r}`)))
 
