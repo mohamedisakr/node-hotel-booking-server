07f754b0773a6faec1c2ac6581bcebe99b89c23d
mohamedisakr <mohamedisakr@gmail.com>
2021-12-31 09:45:29 +0200

    implementing compare password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/user')
 
-const register = async (req, res, next) => {
+export const register = async (req, res, next) => {
   console.log(req.body)
   const {name, email, password} = req.body
 
@@ -37,4 +37,39 @@ const register = async (req, res, next) => {
   }
 }
 
-module.exports = {register}
+export const login = async (req, res, next) => {
+  console.log(req.body)
+  const {email, password} = req.body
+
+  // validation
+  if (!password) {
+    return res.status(400).json({message: 'Password is required'})
+  }
+
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({message: 'Password must be at least 6 characters'})
+  }
+
+  // TODO: email validation
+
+  // check for existing email
+  const userExist = await User.findOne({email}).exec()
+  if (!userExist) {
+    return res.status(404).json({message: `Invalid credentials`})
+  }
+
+  // const newUser = new User({name, email, password})
+  const credentials = {email, password}
+
+  try {
+    const res = await User.findOne({email, password}).exec()
+    console.log(`User created ${newUser}`)
+    return res.status(201).json({data: newUser, message: `User created`})
+  } catch (error) {
+    console.error(`User creation failed ${error}`)
+    return res.status(400).json({message: `User creation failed`})
+  }
+}
+// module.exports = {register}
--- models/user.js
+++ models/user.js
@@ -43,5 +43,16 @@ userSchema.pre('save', function (next) {
   }
 })
 
+userSchema.methods.comparePassword = function (password, next) {
+  bcrypt.compare(password, this.password, function (err, match) {
+    if (err) {
+      console.log(`Compare password error: ${error}`)
+      return next(err, false)
+    }
+    console.log(`Password match: ${match}`)
+    return next(null, match)
+  })
+}
+
 const User = model('user', userSchema)
 module.exports = User
