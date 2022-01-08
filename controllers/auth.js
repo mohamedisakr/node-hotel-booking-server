b1e4479a8ebb971864168f1d2c2a53f68ea4c336
mohamedisakr <mohamedisakr@gmail.com>
2022-01-08 07:22:48 +0200

    fixing headers already sent error

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,33 +3,32 @@ const User = require('../models/user')
 const secret = process.env.JWT_SECRET
 
 export const register = async (req, res, next) => {
-  console.log(req.body)
-  const {name, email, password} = req.body
-
-  // validation
-  if (!name) {
-    return res.status(400).json({message: 'Name is required'})
-  }
-
-  if (!password) {
-    return res.status(400).json({message: 'Password is required'})
-  }
-
-  if (password.length < 6) {
-    return res
-      .status(400)
-      .json({message: 'Password must be at least 6 characters'})
-  }
-
-  // check for existing email
-  const userExist = await User.findOne({email}).exec()
-  if (userExist) {
-    return res.status(400).json({message: `${email} is already exist`})
-  }
-
-  const newUser = new User({name, email, password})
-
   try {
+    console.log(req.body)
+    const {name, email, password} = req.body
+
+    // validation
+    if (!name) {
+      return res.status(400).json({message: 'Name is required'})
+    }
+
+    if (!password) {
+      return res.status(400).json({message: 'Password is required'})
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({message: 'Password must be at least 6 characters'})
+    }
+
+    // check for existing email
+    const userExist = await User.findOne({email}).exec()
+    if (userExist) {
+      return res.status(400).json({message: `${email} is already exist`})
+    }
+
+    const newUser = new User({name, email, password})
     await newUser.save()
     console.log(`User created ${newUser}`)
     return res.status(201).json({data: newUser, message: `User created`})
